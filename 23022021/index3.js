5b8fcb3fa41c53894a893ed3f17552b7f4e85b48
ArturPromon <artur.shovkunenko@gmail.com>
2021-02-23 20:10:46 +0200

    onkeydown

---
+++ 23022021/index3.js
@@ -0,0 +1,21 @@
+function myFunction(){
+  var title = document.getElementById('input1').value;
+  var href = document.getElementById('input2').value;
+  var a = document.createElement("a");
+  a.innerHTML = title;
+  a.href = href;
+  document.getElementById('demo').appendChild(a);
+}
+
+function keyCode(event) {
+  var x = event.keyCode;
+  console.log(x);
+  if (x == 13 || x==27) {
+    var title = document.getElementById('input1').value;
+    var href = document.getElementById('input2').value;
+    var a = document.createElement("a");
+    a.innerHTML = title;
+    a.href = href;
+    document.getElementById('demo').appendChild(a);
+  }
+}
